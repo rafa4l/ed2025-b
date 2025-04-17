d8a3f69b5f49297f6fc325b68f173ce10a374d04
A.J <marinpereirajob@gmail.com>
2025-04-17 16:01:26 -0300

    mensagem sobre atividade

--- JogaJogoDaVelha.java
+++ JogaJogoDaVelha.java
@@ -3,17 +3,22 @@ import java.util.Scanner;
 
 public class JogaJogoDaVelha {
     public static void main(String[] args) {
-        System.out.println("Bem-vindo ao teste do Jogo da Velha!");
+
+        System.out.println("Bem-vindo ao do Jogo da Velha!");
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Digite 1 para jogar ou 0 para sair: ");
+        System.out.println("Digite A para jogar ou B para sair: ");
         int opcao = scanner.nextInt();
         Random random = new Random();
-        while (opcao!=0) {
-            System.out.println("Digite um número inteiro para a dimensão do tabuleiro: ");
+        
+
+        while (opcao != 0) {
+      
+            
+            System.out.println("Digite um número inteiro para poder ter a  dimensão do tabuleiro: ");
             int dimensao = scanner.nextInt();
-            JogoDaVelha jogo = new JogoDaVelha(dimensao);
+            JogoDaVelha jogo = new JogoDaVelha();
             int i, j;
-            while (jogo.venceuUsando().equals("continuar")) {
+            while (jogo.vencedor() == 2) {
                 i = random.nextInt(dimensao);
                 j = random.nextInt(dimensao);
                 try {
@@ -22,16 +27,20 @@ public class JogaJogoDaVelha {
                     System.out.println(e.getMessage());
                     continue;
                 }
-//                System.out.println(jogo.toString());
+                // System.out.println(jogo.toString());
             }
-            if (! jogo.venceuUsando().equals("empate")) {
-                System.out.println("Jogador ganhou utilizando: " + jogo.venceuUsando() );
-            } else  {
+            if (jogo.vencedor() == 1) {
+                System.out.println("Jogador X venceu!");
+            } else if (jogo.vencedor() == -1) {
+                System.out.println("Jogador O venceu!");
+            } else {
                 System.out.println("Empate!");
             }
+            
             System.out.println(jogo.toString());
-            System.out.println("Deseja jogar novamente? (1-sim, 0-nao): ");
+            System.out.println("Deseja jogar novamente? (A-sim, B-nao): ");
             opcao = scanner.nextInt();
         }
     }
 }
+
--- JogoDaVelha.java
+++ JogoDaVelha.java
@@ -1,66 +1,123 @@
 public class JogoDaVelha {
     protected static final int X = 1, O = -1;
     protected static final int VAZIO = 0;
-    protected int tabuleiro[][] = new int[3][3];
+    protected int tabuleiro[][];
     protected int jogador;
+    protected int dimensao;
+   
 
     public JogoDaVelha() {
         limpaTabuleiro();
+        vencedor();
+        
+        tabuleiro = new int[dimensao][dimensao];
+        
     }
 
     public void limpaTabuleiro() {
-        for(int i = 0;i<3;i++) {
-            for (int j=0; j<3; j++) {
-                tabuleiro[i][j]=VAZIO;
+        for (int i = 0; i < dimensao; i++) {
+            for (int j = 0; j < dimensao; j++) {
+                tabuleiro[i][j] = VAZIO;
             }
         }
         jogador = X;
     }
 
     public void poePeca(int i, int j) {
-        if (i<0||i>2||j<0||j>2){
+        if (i < 0 || i >= dimensao || j < 0 || j >= dimensao) {
             throw new IllegalArgumentException("Posição Inválida");
         }
-        if (tabuleiro[i][j]!=VAZIO) throw new IllegalArgumentException("Posição Ocupada");
-        tabuleiro[i][j]=jogador;
+        if (tabuleiro[i][j] != VAZIO)
+            throw new IllegalArgumentException("Posição Ocupada");
+        tabuleiro[i][j] = jogador;
         jogador = -jogador;
     }
 
-
-    public String venceuUsando() {
-        String resultado = "continuar";
-        // Implemente este método que deve retornar se o vencedor venceu com
-        // uma linha, coluna ou diagonal. O retorno deve ser "linha", "coluna",
-        // "diagonal" ou "continuar" caso o jogo não tenha terminado.
-
-        return resultado;
+    public boolean descVencedor(int marca) {
+        for (int i = 0; i < dimensao; i++) {
+            int somaLinha = 0;
+            for (int j = 0; j < dimensao; j++) {
+                somaLinha += tabuleiro[i][j];
+            }
+            if (somaLinha == marca * dimensao) {
+                return true;
+            }
+        }
+        for (int j = 0; j < dimensao; j++) {
+            int somaColuna = 0;
+            for (int i = 0; i < dimensao; i++) {
+                somaColuna += tabuleiro[i][j];
+            }
+            if (somaColuna == marca * dimensao) {
+                return true;
+            }
+        }
+        int somaDiagonal1 = tabuleiro[0][0] + tabuleiro[1][1] + tabuleiro[2][2];
+        if (somaDiagonal1 == marca * dimensao -1 ) {
+            return true;
+        }
+        int somaDiagonal2 = tabuleiro[2][0] + tabuleiro[1][1] + tabuleiro[0][2];
+        if (somaDiagonal2 == marca * dimensao -1) {
+            return true;
+        }
+        return false;
     }
-    
+
+    public int vencedor() {
+        if (descVencedor(1)) {
+            return (1);
+        } else if (descVencedor(2)) {
+            return (-1);
+        } else if(descVencedor(O)){
+            return (0);
+        }else 
+        return(2);
+    }
+
     public String toString() {
-        /** Implementar o método to String que deve retornar
-         * uma string com o tabuleiro do jogo da velha com as peças
-         * nas posições corretas.
-         */
+        String s = "";
+        for (int i = 0; i <= dimensao; i++) {
+            for (int j = 0; j <=dimensao; j++) {
+                switch (tabuleiro[i][j]) {
+                    case X:
+                        s += "X";
+                        break;
+                    case O:
+                        s += "O";
+                        break;
+                    case VAZIO:
+                        s += " ";
+                        break;
+                }
+                if (j < dimensao)
+                    s += ("|");
+                if (i < dimensao)
+                    s +=("\n");
+                return s;
+            }
+        }
+
         String retorno = "";
-        for (int i=0; i<3;i++){
-            for (int j=0; j<3; j++){
-                if(tabuleiro[i][j]==X) {
+
+        for (int i = 0; i <= dimensao; i++) {
+            for (int j = 0; j < dimensao; j++) {
+                if (tabuleiro[i][j] == X) {
                     retorno += ("X");
-                } else if (tabuleiro[i][j]==O) {
+                } else if (tabuleiro[i][j] == O) {
                     retorno += ("O");
                 } else {
                     retorno += (" ");
                 }
-                if (j<2){
+                if (j < dimensao) {
                     retorno += ("|");
                 }
             }
-            //System.out.println();
-            if (i<2){
+            // System.out.println();
+            if (i < 2) {
                 retorno += ("\n-----\n");
             }
 
-        }   
+        }
         return retorno;
     }
 }
